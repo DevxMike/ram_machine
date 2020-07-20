987d63952e7ee75024d58b4a18ccfa15b09937b3
Mike <michalbaz@poczta.onet.pl>
2020-07-21 01:16:47 +0200

    main changed

---
+++ errors.h
@@ -0,0 +1,7 @@
+#ifndef errors_h_
+#define errors_h_
+
+unsigned STACK_INIT_ERR = 0x01;
+
+
+#endif
--- main.c
+++ main.c
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include "interpreter.h"
 #include "stack.h"
+#include "errors.h"
 
-int main() 	//------------------------------------------------main S
-{
+int main(int argc, char** argv){
+    unsigned EXIT_CODE = 0;
+
     char* m_str;
     call_stack_t* call_stack = NULL;
+    AnalyzedData data;
     if((call_stack = init_stack(50)) != NULL){
         printf("stack initialized\n\n");
         print_stack_status(call_stack);
-        AnalyzedData data0;
         m_str = UserInputToString();
         printf("\n\nYOUR INPUT: ");
         PrintString(m_str);
         printf("\n\nDATA TYPE:\n");
-        if(DataTypeAnalyzer(&data0, m_str)){
-            Interpreter(&data0);
+        if(DataTypeAnalyzer(&data, m_str)){
+            Interpreter(&data);
         }
         else{
             printf("rare bug has just occured, good luck with that!\n");
@@ -27,8 +29,12 @@ int main() 	//------------------------------------------------main S
     }
     else{
         printf("failed to initialize call stack!\n");
-        exit(0x01);
+        EXIT_CODE = STACK_INIT_ERR;
     }
 
-    return 0;
-}			//------------------------------------------------main E
+    printf("exit code: 0x%04x\n", EXIT_CODE);
+    if(EXIT_CODE > 0){
+        printf("Check documentation/readme for error numbers description.\n");
+    }
+    return EXIT_CODE;
+}
