ab2785bd285fb3b5f0de094d3ea4775615d90044
Mike <michalbaz@poczta.onet.pl>
2020-07-21 15:52:48 +0200

    task_queue_init

--- task_queue.h
+++ task_queue.h
@@ -1,7 +1,27 @@
 #ifndef task_queue_h
 #define task_queue_h
 #include "stack.h"
+#include <stdlib.h>
 
 typedef stack_data_t task_queue_data_t;
+typedef struct{
+    size_t max;
+    size_t quantity;
+    task_queue_data_t* head, * tail;
+}task_queue_t;
+
+task_queue_t* task_queue_init(size_t size){
+    task_queue_t* temp = NULL;
+    if((temp = (task_queue_t*)malloc(sizeof(task_queue_t))) != NULL){
+        temp->head = temp->tail = NULL;
+        temp->max = size;
+        temp->quantity = 0;
+        return temp;
+    }
+    else{
+        return NULL;
+    }
+}
+
 
 #endif
