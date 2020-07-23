99ea1e106235318d1497426e0eb61c7459528e00
Mike <michalbaz@poczta.onet.pl>
2020-07-23 16:03:42 +0200

    change

--- reader.c
+++ reader.c
@@ -21,7 +21,7 @@ bool has_invalid_chars(char* pt){
 
 
 //for tests
-const int size = 9;
+const size_t size = 9;
 const char temp[][30] = {
         "1",
         "1.5",
@@ -34,15 +34,16 @@ const char temp[][30] = {
         "LOAD *5"
 };
 
-char** read_file(char* f_name){
+char** read_file(char* f_name, size_t* sizeof_arr){
     char** tasks = NULL;
     //change the body of this function
     //test cases
     tasks = (char**)malloc(sizeof(char*) * size);
-    for(int i = 0; i < size; ++i){
+    for(size_t i = 0; i < size; ++i){
         tasks[i] = (char*)malloc(sizeof(char) * strlen(temp[i]) + 1);
         strcpy(tasks[i], temp[i]);
     }
+    *sizeof_arr = size;
     //end of test cases
     return tasks;
 }
