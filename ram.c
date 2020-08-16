7ca805cae1463c9e8aa1027ab52ca56c09190cb1
Mike <michalbaz@poczta.onet.pl>
2020-08-17 00:36:01 +0200

    swap and init ready

--- ram.c
+++ ram.c
@@ -1,1 +1,30 @@
 #include "ram.h"
+#include <stdlib.h>
+
+void swap_structs(ram_cell_t* r1, ram_cell_t* r2){
+    ram_cell_t temp;
+    temp.cell_id = r1->cell_id;
+    temp.value = r1->value;
+    r1->cell_id = r2->cell_id;
+    r1->value = r2->value;
+    r2->cell_id = r2->cell_id;
+    r2->value = r2->value;
+}
+
+inline int greater(const id_t id1, const id_t id2){
+    return id1 > id2;
+}
+ram_chip_t* init_ram(){
+    ram_chip_t* temp = NULL;
+    if((temp = (ram_chip_t*) malloc(sizeof(ram_cell_t))) != NULL){
+        temp->chip_size = 1;
+    }
+
+    return temp;
+}
+void ram_push(ram_chip_t* r_chip, const ram_cell_t* r_cell){
+    //to do
+}
+ram_cell_t* ram_pop(void){
+    return NULL; //to do
+}
--- ram.h
+++ ram.h
@@ -2,16 +2,21 @@
 #define ram_h
 
 #include <stdio.h>
+typedef unsigned long long id_t;
 typedef struct{
-    unsigned long long cell_id; //creating i.e. 3 "ram cells" is much better than not using 10000 ram cells  
+    id_t cell_id; //creating i.e. 3 "ram cells" is much better than not using 10000 ram cells  
     int value; //value to be hold
 }ram_cell_t;
 
 typedef struct{
-    size_t quantity; //quantity of ram cells used in user`s algorithm
+    size_t chip_size; //quantity of ram cells used in user`s algorithm
     ram_cell_t* arr; //array of "ram cells"
 }ram_chip_t;
 
+ram_chip_t* init_ram();
+void ram_push(ram_chip_t*, const ram_cell_t*);
+ram_cell_t* ram_pop(void);
 
 
+void swap_structs(ram_cell_t*, ram_cell_t*);
 #endif
