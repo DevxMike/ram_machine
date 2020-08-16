ea4a25a9e7258d426c25de443e8e088efcbe0624
Mike <michalbaz@poczta.onet.pl>
2020-08-16 22:50:00 +0200

    data structures ready

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include "errors.h"
 #include "syntax.h"
+#include "ram.h"
 
 int main(int argc, char** argv){
     unsigned EXIT_CODE = 0;
--- ram.h
+++ ram.h
@@ -1,4 +1,17 @@
 #ifndef ram_h
 #define ram_h
 
+#include <stdio.h>
+typedef struct{
+    unsigned long long cell_id; //creating i.e. 3 "ram cells" is much better than not using 10000 ram cells  
+    int value; //value to be hold
+}ram_cell_t;
+
+typedef struct{
+    size_t quantity; //quantity of ram cells used in user`s algorithm
+    ram_cell_t* arr; //array of "ram cells"
+}ram_chip_t;
+
+
+
 #endif
