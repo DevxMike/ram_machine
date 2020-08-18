c5888083144d7afcb3b5a6ded530e25e783820de
Mike <michalbaz@poczta.onet.pl>
2020-08-18 14:34:08 +0200

    function headers

--- heap.h
+++ heap.h
@@ -6,7 +6,13 @@ typedef ram_cell_t heap_data_t;
 typedef struct{
     size_t elements;
     heap_data_t* arr;
-};
+}heap_t;
 
+void h_push(heap_t*, const heap_data_t*);
+heap_data_t* h_pop(heap_t*);
+heap_t* init_heap(void);
 
+inline int heap_empty(const heap_t* heap){
+    return heap->elements == 0;
+}
 #endif
