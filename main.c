b3cde5753b8f72e6a9b844bf6e57b1603a9ee98d
Mike <michalbaz@poczta.onet.pl>
2020-08-20 16:33:08 +0200

    changes in main, added a data struct holding all program vars

--- main.c
+++ main.c
@@ -1,36 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "interpreter.h"
-#include "stack.h"
-#include "task_queue.h"
-#include <string.h>
-#include "reader.h"
-#include <stdbool.h>
-#include "errors.h"
-#include "syntax.h"
-#include "ram.h"
-#include "ram_heap.h"
-
-ram_cell_t R0 = {
-    0, 0
-};
+#include "main.h"
 
 int main(int argc, char** argv){
-    unsigned EXIT_CODE = 0;
-    char* m_str;
-    call_stack_t* call_stack = NULL;
-    task_queue_t* task_queue = NULL;
+    ram_cell_t R0 = {
+    0, 0
+    };
+    main_vars_t program_variables;
+    init_main(&program_variables);
     task_queue_element_t* temp;
     task_queue_data_t* t_data = NULL;
-    AnalyzedData data;
-    size_t task_arr_size = 0, input_data_size = 0;
-    char** task_arr = NULL;
-    int* input_tab = NULL;
-    bool empty_input = true;
-
-    ram_heap_t* ram_heap = NULL, *ram_heap_copy = NULL;
-    ram_chip_t* ram_chip = NULL;
-
 
     if(argc == 1){ //if argv contains program name only
         printf("Wrong usage. Try %s <file_name>.txt or %s -h for help\n", argv[0], argv[0]);
@@ -40,159 +17,166 @@ int main(int argc, char** argv){
         
         if(strcmp(argv[1], "-h") == 0){ //if argv contains program name and "-h" string 
             print_help(argv[0], COMMAND_ROW, commands, COMMAND_ROW, ex_operands);
-            exit_w_code(EXIT_CODE);
+            exit_w_code(program_variables.EXIT_CODE);
         } 
         else if(has_invalid_chars(argv[1], "txt")){ //if <file_name> string has forbidden chars
             exit_w_code(FILE_NAME_ERR);
         }
 
         if(has_loops){ 
-            if((call_stack = init_stack(50)) == NULL){
+            if((program_variables.stack = init_stack(50)) == NULL){
                 exit_w_code(STACK_INIT_ERR);
             }
         }
         printf("file to be read: %s\n\n", argv[1]);
 
-        if((task_arr = read_file(argv[1], &task_arr_size, &EXIT_CODE)) != NULL && task_arr_size > 0){
-            if(((ram_heap = init_ram_heap()) == NULL) || ((ram_heap_copy = init_ram_heap()) == NULL)){ //init heap used to heap-sort ram chip
+        if((program_variables.task_arr.task_arr = read_file(argv[1], &program_variables.task_arr.arr_size, &program_variables.EXIT_CODE)) != NULL && program_variables.task_arr.arr_size > 0){
+            if(((program_variables.ram_heap = init_ram_heap()) == NULL) || ((program_variables.ram_heap_copy = init_ram_heap()) == NULL)){ //init heap used to heap-sort ram chip
                 exit_w_code(HEAP_INIT_ERR);
             }
-            if((ram_chip = init_ram()) == NULL){ //init ram chip
+            if((program_variables.ram_chip = init_ram()) == NULL){ //init ram chip
                 exit_w_code(RAM_INIT_ERR);
             }
-            if((task_queue = task_queue_init(task_arr_size)) == NULL){ //safety, it`s better to have more "place", can be easilly changed 
+            if((program_variables.queue = task_queue_init(program_variables.task_arr.arr_size)) == NULL){ //safety, it`s better to have more "place", can be easilly changed 
                 exit_w_code(TASK_QUEUE_INIT_ERR);
             }
 
-            ram_push(ram_chip, &R0); //add special register to ram chip
-            ram_heap_push(ram_heap_copy, &R0, &EXIT_CODE); //only one register available, no need to sort 
+            ram_push(program_variables.ram_chip, &R0); //add special register to ram chip
+            ram_heap_push(program_variables.ram_heap_copy, &R0, &program_variables.EXIT_CODE); //only one register available, no need to sort 
 
-            for(size_t i = 0; i < task_arr_size; ++i){
-                if(DataTypeAnalyzer(&data, task_arr[i])){
-                    if(Interpreter(&data, task_queue, &EXIT_CODE, i) == 1){
-                        input_tab = input_data(data.data, &EXIT_CODE, &input_data_size);
+            for(size_t i = 0; i < program_variables.task_arr.arr_size; ++i){
+                if(DataTypeAnalyzer(&program_variables.data, program_variables.task_arr.task_arr[i])){
+                    if(Interpreter(&program_variables.data, program_variables.queue, &program_variables.EXIT_CODE, i) == 1){
+                        program_variables.input.data_arr = input_data(program_variables.data.data, &program_variables.EXIT_CODE, &program_variables.input.arr_size);
                     } 
-                    if(EXIT_CODE){
-                        if(call_stack != NULL){
-                        free(call_stack->data);//free memory
-                        free(call_stack);
+                    if(program_variables.EXIT_CODE){
+                        if(program_variables.stack != NULL){
+                        free(program_variables.stack->data);//free memory
+                        free(program_variables.stack);
                         }
-                        for(size_t i = 0; i < task_arr_size; ++i){
-                            free(task_arr[i]);
-                        }
-                        free(task_arr);
                         //free task queue
-                        if(!task_queue_empty(task_queue)){
-                            while(task_queue->head != NULL){
-                                temp = task_queue->head;
-                                task_queue->head = task_queue->head->next;
+                        if(!task_queue_empty(program_variables.queue)){
+                            while(program_variables.queue->head != NULL){
+                                temp = program_variables.queue->head;
+                                program_variables.queue->head = program_variables.queue->head->next;
                                 free(temp);
                             }
                         }  
-                        for(size_t i = 0; i < task_arr_size; ++i){
-                            free(task_arr[i]);
+                       for(size_t i = 0; i < program_variables.task_arr.arr_size; ++i){
+                            free(program_variables.task_arr.task_arr[i]);
                         }
-                        free(ram_chip->arr);
-                        free(ram_chip);
-                        free(ram_heap_copy->arr);
-                        free(ram_heap_copy);
-                        free(ram_heap->arr);
-                        free(ram_heap);
-                        free(task_arr);
-                        free(task_queue); 
-                        exit_w_code(EXIT_CODE); //syntax err
+                        free(program_variables.task_arr.task_arr);
+                        free(program_variables.ram_chip->arr);
+                        free(program_variables.ram_chip);
+                        free(program_variables.ram_heap_copy->arr);
+                        free(program_variables.ram_heap_copy);
+                        free(program_variables.ram_heap->arr);
+                        free(program_variables.ram_heap);
+                        free(program_variables.queue); 
+                        exit_w_code(program_variables.EXIT_CODE); //syntax err
                     }
                 }
                 else{
                     /*----------------------------------------------if failed free memory section -----------------------------------------------*/
-                    if(call_stack != NULL){
-                        free(call_stack->data);//free memory
-                        free(call_stack);
-                    }
-                    for(size_t i = 0; i < task_arr_size; ++i){
-                        free(task_arr[i]);
-                    }
-                    free(task_arr);
-                    //free task queue
-                    if(!task_queue_empty(task_queue)){
-                        while(task_queue->head != NULL){
-                            temp = task_queue->head;
-                            task_queue->head = task_queue->head->next;
-                            free(temp);
+                    if(program_variables.stack != NULL){
+                        free(program_variables.stack->data);//free memory
+                        free(program_variables.stack);
                         }
-                    }  
-                    for(size_t i = 0; i < task_arr_size; ++i){
-                        free(task_arr[i]);
-                    }
-                    free(ram_chip->arr);
-                    free(ram_chip);
-                    free(ram_heap_copy->arr);
-                    free(ram_heap_copy);
-                    free(ram_heap->arr);
-                    free(ram_heap);
-                    free(task_arr);
-                    free(task_queue); 
+                        //free task queue
+                        if(!task_queue_empty(program_variables.queue)){
+                            while(program_variables.queue->head != NULL){
+                                temp = program_variables.queue->head;
+                                program_variables.queue->head = program_variables.queue->head->next;
+                                free(temp);
+                            }
+                        }  
+                       for(size_t i = 0; i < program_variables.task_arr.arr_size; ++i){
+                            free(program_variables.task_arr.task_arr[i]);
+                        }
+                        free(program_variables.task_arr.task_arr);
+                        free(program_variables.ram_chip->arr);
+                        free(program_variables.ram_chip);
+                        free(program_variables.ram_heap_copy->arr);
+                        free(program_variables.ram_heap_copy);
+                        free(program_variables.ram_heap->arr);
+                        free(program_variables.ram_heap);
+                        free(program_variables.queue); 
                     exit_w_code(WRONG_SYNTAX_ERR);
                     /*----------------------------------------------if failed free memory section -----------------------------------------------*/
                 }
             }
-            while(!task_queue_empty(task_queue)){
-                t_data = q_pop(task_queue);
-                tasker(ram_chip, t_data);
+            while(!task_queue_empty(program_variables.queue)){
+                t_data = q_pop(program_variables.queue);
+                tasker(program_variables.ram_chip, t_data);
                 free(t_data);
             }
         }
         else{
             /*----------------------------------------------if failed free memory section -----------------------------------------------*/
-            if(call_stack != NULL){
-                free(call_stack->data);//free memory
-                free(call_stack);
+            if(program_variables.stack != NULL){
+                free(program_variables.stack->data);//free memory
+                free(program_variables.stack);
             }
             //free task queue
-            if(!task_queue_empty(task_queue)){
-                while(task_queue->head != NULL){
-                    temp = task_queue->head;
-                    task_queue->head = task_queue->head->next;
+            if(!task_queue_empty(program_variables.queue)){
+                while(program_variables.queue->head != NULL){
+                    temp = program_variables.queue->head;
+                    program_variables.queue->head = program_variables.queue->head->next;
                     free(temp);
                 }
             }  
-            free(ram_chip->arr);
-            free(ram_chip);
-            free(ram_heap_copy->arr);
-            free(ram_heap_copy);
-            free(ram_heap->arr);
-            free(ram_heap);
-            free(task_queue); 
+            for(size_t i = 0; i < program_variables.task_arr.arr_size; ++i){
+                free(program_variables.task_arr.task_arr[i]);
+            }
+            free(program_variables.task_arr.task_arr);
+            free(program_variables.ram_chip->arr);
+            free(program_variables.ram_chip);
+            free(program_variables.ram_heap_copy->arr);
+            free(program_variables.ram_heap_copy);
+            free(program_variables.ram_heap->arr);
+            free(program_variables.ram_heap);
+            free(program_variables.queue); 
             /*----------------------------------------------if failed free memory section -----------------------------------------------*/
-            exit_w_code(EXIT_CODE);
+            exit_w_code(program_variables.EXIT_CODE);
         }
 
 
         /*----------------------------------------------------free memory section----------------------------------------------------------*/
-        if(call_stack != NULL){
-                free(call_stack->data);//free memory
-                free(call_stack);
+        if(program_variables.stack != NULL){
+            free(program_variables.stack->data);//free memory
+            free(program_variables.stack);
         }
-        for(size_t i = 0; i < task_arr_size; ++i){
-            free(task_arr[i]);
-        }
-        free(task_arr);
-            //free task queue
-        if(!task_queue_empty(task_queue)){
-            while(task_queue->head != NULL){
-                temp = task_queue->head;
-                task_queue->head = task_queue->head->next;
+        //free task queue
+        if(!task_queue_empty(program_variables.queue)){
+            while(program_variables.queue->head != NULL){
+                temp = program_variables.queue->head;
+                program_variables.queue->head = program_variables.queue->head->next;
                 free(temp);
             }
         }  
-        free(ram_chip->arr);
-        free(ram_chip);
-        free(ram_heap_copy->arr);
-        free(ram_heap_copy);
-        free(ram_heap->arr);
-        free(ram_heap); 
-        free(task_queue); 
+        for(size_t i = 0; i < program_variables.task_arr.arr_size; ++i){
+            free(program_variables.task_arr.task_arr[i]);
+        }
+        free(program_variables.task_arr.task_arr);
+        free(program_variables.ram_chip->arr);
+        free(program_variables.ram_chip);
+        free(program_variables.ram_heap_copy->arr);
+        free(program_variables.ram_heap_copy);
+        free(program_variables.ram_heap->arr);
+        free(program_variables.ram_heap);
+        free(program_variables.queue); 
         /*----------------------------------------------------free memory section----------------------------------------------------------*/
-        exit_w_code(EXIT_CODE);  
+        exit_w_code(program_variables.EXIT_CODE);
     }    
+}
+void init_main(main_vars_t* vars){
+    vars->EXIT_CODE = 0;
+    vars->input.data_arr = NULL;
+    vars->queue = NULL;
+    vars->ram_chip = NULL;
+    vars->ram_heap = NULL;
+    vars->ram_heap_copy = NULL;
+    vars->stack = NULL;
+    vars->task_arr.task_arr = NULL;
+    vars->input.arr_size = vars->input.element = vars->task_arr.arr_size = 0;   
 }
--- main.h
+++ main.h
@@ -13,9 +13,29 @@
 
 #ifndef main_h_
 #define main_h_
+
 typedef struct{
-    
+    int* data_arr;
+    size_t arr_size;
+    size_t element;
+}input_data_t;
+
+typedef struct{
+    char** task_arr;
+    size_t arr_size;
+}tasks_t;
+typedef struct{
+    unsigned EXIT_CODE;
+    call_stack_t* stack;
+    task_queue_t* queue;
+    AnalyzedData data;
+    input_data_t input;
+    tasks_t task_arr;
+    ram_heap_t* ram_heap;
+    ram_heap_t* ram_heap_copy;
+    ram_chip_t* ram_chip;
 }main_vars_t;
+void init_main(main_vars_t*);
 
 
 #endif
