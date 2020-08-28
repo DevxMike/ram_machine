89ab2c60833137752f5ac10794190129538330cf
Mike <michalbaz@poczta.onet.pl>
2020-08-28 22:49:07 +0200

    simple data structures

--- loop.h
+++ loop.h
@@ -1,4 +1,17 @@
 #ifndef loop_h
 #define loop_h
+#include "loop_list.h"
+
+typedef struct{
+    char* loop_et;
+    list_element_t* task_list;
+}loop_element_t;
+
+typedef struct{
+    size_t quantity;
+    loop_element_t* arr;
+}loop_t;
+
+extern char** loops;
 
 #endif
