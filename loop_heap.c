f4614d1cc31635262d3a3f30ff92e0d69ba9a1f2
Mike <michalbaz@poczta.onet.pl>
2020-09-02 21:31:06 +0200

    headers and hollow bodies

--- loop_heap.c
+++ loop_heap.c
@@ -1,1 +1,16 @@
 #include "loop_heap.h"
+loop_heap_t* init_heap(){
+    return NULL;
+}
+void loop_heap_push(loop_heap_t* heap, const loop_t* el){
+
+}
+void loop_heap_pop(loop_heap_t* heap, loop_container_t* container){
+
+}
+int loop_heap_empty(const loop_heap_t* heap){
+    return 0;
+}
+int loop_heap_full(const loop_heap_t* heap){
+    return 0;
+}
--- loop_heap.h
+++ loop_heap.h
@@ -1,4 +1,17 @@
 #ifndef loop_heap_h
 #define loop_heap_h
+#include "loop.h"
+#include <stdio.h>
 
+typedef struct{
+    loop_t* arr;
+    size_t size;
+    size_t quantity;
+}loop_heap_t;
+
+loop_heap_t* init_heap(void);
+void loop_heap_push(loop_heap_t*, const loop_t*);
+void loop_heap_pop(loop_heap_t*, loop_container_t*);
+int loop_heap_empty(const loop_heap_t*);
+int loop_heap_full(const loop_heap_t*);
 #endif
