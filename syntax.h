34dc580548955aa311505879c086d1b5ccf9d000
Mike <michalbaz@poczta.onet.pl>
2020-09-09 22:21:24 +0200

    syntax updated

--- syntax.h
+++ syntax.h
@@ -1,12 +1,10 @@
 #ifndef syntax_h_
 #define syntax_h_
 #include <stdio.h>
-#define COMMAND_ROW 15
-#define COMMAND_COL 15  
+#define COMMAND_ROW 26 
 
 void print_help(const char*, size_t, const char*[], size_t, const char*[]);
 extern const char* commands[COMMAND_ROW];
 extern const char* ex_operands[COMMAND_ROW];
-extern void(*operation[COMMAND_ROW])(void); //to do
 
 #endif
